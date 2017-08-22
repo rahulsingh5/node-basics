1daa994a134098a4ade8380e6957cfd00936ddd4
Rahul Singh <rahul@Rahuls-MacBook-Pro.local>
2017-08-22 21:22:42 +0530

    starting with express server

--- application.js
+++ application.js
@@ -1,25 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
-var mongodb_1 = require("mongodb");
-var assert = require("assert");
 var config_1 = require("./config");
 var app = express();
-mongodb_1.MongoClient.connect("mongodb:" + config_1.config.mongodbURL + "/" + config_1.config.dbName, function (error, db) {
-    assert.equal(null, error);
-    console.log("Application Server connected to " + config_1.config.dbName + " database");
-    db.collection('movies').find({}).toArray(function (error, docs) {
-        if (docs.length === 0) {
-            console.log('There is no documents found');
-            db.close();
-            return;
-        }
-        docs.forEach(function (doc) {
-            console.log("The movie title is " + doc.title);
-        });
-        db.close();
-    });
-    console.log("Called find() for movies");
+app.get('/', function (req, res) {
+    res.send('Hello from express js');
+});
+app.use(function (req, res) {
+    res.sendStatus(404);
 });
 app.listen(config_1.config.port, function () {
     console.log("Server is running at http://localhost:" + config_1.config.port);
--- application.ts
+++ application.ts
@@ -1,32 +1,16 @@
 import * as express from 'express';
-import { MongoClient } from 'mongodb';
-
-import * as assert from 'assert';
+import { Request, Response } from 'express';
 
 import { config } from './config';
 
 const app = express();
 
-MongoClient.connect(`mongodb:${config.mongodbURL}/${config.dbName}`, (error: any, db: any) => {
-    assert.equal(null, error);
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from express js');
+});
 
-    console.log(`Application Server connected to ${config.dbName} database`);
-    db.collection('movies').find({}).toArray((error: any, docs: any) => {
-
-        if (docs.length === 0) {
-            console.log('There is no documents found');
-            db.close();
-            return;
-        }
-
-        docs.forEach((doc: any) => {
-            console.log(`The movie title is ${doc.title}`);
-        });
-
-        db.close();
-    });
-
-    console.log(`Called find() for movies`);
+app.use((req: Request, res: Response) => {
+    res.sendStatus(404);
 });
 
 app.listen(config.port, () => {
