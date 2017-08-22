5dc9e38ec07cefec035ffd964b84a7920a573865
Rahul Singh <rahul@Rahuls-MacBook-Pro.local>
2017-08-22 20:44:24 +0530

    added mongoclient to test db docs

--- application.js
+++ application.js
@@ -8,6 +8,18 @@ var app = express();
 mongodb_1.MongoClient.connect("mongodb:" + config_1.config.mongodbURL + "/" + config_1.config.dbName, function (error, db) {
     assert.equal(null, error);
     console.log("Application Server connected to " + config_1.config.dbName + " database");
+    db.collection('movies').find({}).toArray(function (error, docs) {
+        if (docs.length === 0) {
+            console.log('There is no documents found');
+            db.close();
+            return;
+        }
+        docs.forEach(function (doc) {
+            console.log("The movie title is " + doc.title);
+        });
+        db.close();
+    });
+    console.log("Called find() for movies");
 });
 app.listen(config_1.config.port, function () {
     console.log("Server is running at http://localhost:" + config_1.config.port);
--- application.ts
+++ application.ts
@@ -11,6 +11,22 @@ MongoClient.connect(`mongodb:${config.mongodbURL}/${config.dbName}`, (error: any
     assert.equal(null, error);
 
     console.log(`Application Server connected to ${config.dbName} database`);
+    db.collection('movies').find({}).toArray((error: any, docs: any) => {
+
+        if (docs.length === 0) {
+            console.log('There is no documents found');
+            db.close();
+            return;
+        }
+
+        docs.forEach((doc: any) => {
+            console.log(`The movie title is ${doc.title}`);
+        });
+
+        db.close();
+    });
+
+    console.log(`Called find() for movies`);
 });
 
 app.listen(config.port, () => {
